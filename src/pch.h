5756cd07529862ea7ac10a3450c52804cd038a1a
Torbjørn Årdal <torbjorn.aardal@gmail.com>
2024-06-20 21:46:27 +0200

    Added precompiled header

---
+++ src/pch.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// Standard library
+#include <exception>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+// App
+#include "system/assert.h"
+#include "system/error.h"
+#include "system/error_signal.h"
+#include "system/environment.h"
+#include "system/log.h"
+#include "system/numbers.h"
+#include "system/ref.h"
+#include "system/utils.h"
